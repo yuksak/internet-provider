1c530deaf1d5d5e4cd32340be0351ec2c792c743
yuksak <gulamaier@gmail.com>
2023-01-18 14:35:15 +0500

    feat: add page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import { useState } from 'react'
-import istvLogo from './assets/istv-logo.svg'
+import { Route, Routes } from 'react-router-dom'
+
+import { Layout } from 'components'
+import { MainPage, NotFoundPage } from 'pages'
+
+const routes = [
+  { path: '*', component: <NotFoundPage /> },
+  { path: '/', component: <MainPage /> },
+]
 
 function App() {
-  return <img src={istvLogo} />
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        {routes.map(({ path, component }) => (
+          <Route key={path} path={path} element={component} />
+        ))}
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
