d8dc39923a5490eefdba4de9b5740dc392758b96
yuksak <gulamaier@gmail.com>
2023-01-19 23:36:11 +0500

    feat: create reusable section title

---
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react'
+
+interface ISectionTitle {
+  title: string
+  classes?: string
+}
+
+export const SectionTitle: FC<ISectionTitle> = ({ title, classes }) => {
+  return (
+    <h1
+      className={`${classes} text-2xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:text-5xl py-4 md:py-8`}
+    >
+      <span className="underline underline-offset-3 decoration-4 sm:decoration-8 decoration-blue-400 ">
+        {title}
+      </span>
+    </h1>
+  )
+}
