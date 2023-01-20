bbcb79cb7b388c3e23aef391a73086fde7baac86
yuksak <gulamaier@gmail.com>
2023-01-21 00:05:33 +0500

    feat: create header, combine all components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useScrollPosition } from 'hooks/useScrollPosition'
+
+import { NavbarTop } from './NavbarTop'
+import { NavbarBottom } from './NavbarBottom'
+
+export const Header = () => {
+  const { scrollPosition } = useScrollPosition()
+  const isScrollTrue = scrollPosition > 35
+
+  return (
+    <>
+      <header
+        className={`anim transition-all ease-in duration-500 fixed left-0 w-full z-50 ${
+          isScrollTrue ? '-top-[90px] md:-top-[37px]' : 'top-0'
+        }`}
+      >
+        <NavbarTop />
+        <NavbarBottom />
+      </header>
+      <div className="mt-36 md:mt-28" />
+    </>
+  )
+}
