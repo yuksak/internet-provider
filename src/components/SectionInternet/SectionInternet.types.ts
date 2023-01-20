74fcbca8e67fe39af5a022fc1082532a41586c02
yuksak <gulamaier@gmail.com>
2023-01-21 00:01:09 +0500

    refactor: create types of section internet

---
+++ src/components/SectionInternet/SectionInternet.types.ts
@@ -0,0 +1,13 @@
+export interface ICard {
+  title: string
+  price: string
+  speedAfter1?: string
+  speedAfter20?: string
+  tasix: string
+  istv: string
+  iptv: string
+  limit?: string
+  newBadge?: boolean
+  speed?: string
+  priceForOneMb?: string
+}
