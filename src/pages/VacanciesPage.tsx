cd271e5dda8db6a0175bfa9cc45cf12e966135bf
yuksak <gulamaier@gmail.com>
2023-01-23 18:31:36 +0500

    feat: create page for vacancies

---
+++ src/pages/VacanciesPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AccordionList, SectionTitle } from 'components'
+
+export const VacanciesPage = () => {
+  return (
+    <div className="container mx-auto mb-96 px-2">
+      <SectionTitle title="Ish o'rinlari" />
+      <AccordionList />
+    </div>
+  )
+}
