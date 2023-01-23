d1ab6b586aab7206d3c46b1ab1b182ddf8353833
yuksak <gulamaier@gmail.com>
2023-01-23 18:32:04 +0500

    refactor: update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { Layout } from 'components'
-import { MainPage, NotFoundPage } from 'pages'
+import { MainPage, NotFoundPage, VacanciesPage } from 'pages'
 
 const routes = [
   { path: '*', component: <NotFoundPage /> },
   { path: '/', component: <MainPage /> },
+  { path: '/vacancies', component: <VacanciesPage /> },
 ]
 
 function App() {
