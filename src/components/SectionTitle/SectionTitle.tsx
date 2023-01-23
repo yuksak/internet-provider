1e779079fc8df9fc6f95b17eb5c96b1e703b140d
yuksak <gulamaier@gmail.com>
2023-01-23 18:39:26 +0500

    refactor: change style, import types

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,14 +1,12 @@
 import React, { FC } from 'react'
+import { ISectionTitleProps } from './SectionTitle.types'
 
-interface ISectionTitle {
-  title: string
-  classes?: string
-}
-
-export const SectionTitle: FC<ISectionTitle> = ({ title, classes }) => {
+export const SectionTitle: FC<ISectionTitleProps> = ({ title, classes }) => {
   return (
     <h1
-      className={`${classes} text-2xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:text-5xl py-4 md:py-8`}
+      className={`${
+        classes ? classes : ''
+      } text-2xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:text-5xl py-4 md:py-8`}
     >
       <span className="underline underline-offset-3 decoration-4 sm:decoration-8 decoration-blue-400 ">
         {title}
