490d41ce7709e4df6245cd4175b8f8fad05f7f77
yuksak <gulamaier@gmail.com>
2023-01-23 18:41:05 +0500

    refactor: change style, import types

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,13 @@
 import React, { FC } from 'react'
+import { IButtonProps } from './Button.types'
 
-interface IButtonProps {
-  name: string
-  onClick?: () => void
-  children?: React.ReactNode
-}
-
-export const Button: FC<IButtonProps> = ({ name, onClick, children }) => {
+export const Button: FC<IButtonProps> = ({ name, onClick, children, className }) => {
   return (
     <button
       type="button"
-      className="text-white ease-in duration-200 w-full text-center hover:text-blue-500 uppercase flex items-center justify-center gap-0.5 bg-blue-500 hover:bg-white font-medium text-xs px-2 sm:px-5 py-2.5 mr-2 md:mr-0"
+      className={`${
+        className ? className : ''
+      } text-white ease-in duration-200 text-center hover:text-blue-500 uppercase flex items-center justify-center gap-0.5 bg-blue-500 hover:bg-white font-medium text-xs px-2 py-1 sm:px-5 sm:py-2.5 mr-2 md:mr-0`}
       onClick={onClick}
     >
       {children && children}
